78ac1d9788cb21c29d331a36954edcdcdc80957f
Anil <anil.iitk@gmail.com>
2024-08-24 12:07:54 +0530

    WebSocket Hello World
    Websocket hello world

---
+++ initial/src/main/java/com/example/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,25 @@
+package com.example.messagingstompwebsocket;
+
+public class Greeting {
+	
+	private String content;
+	
+	public Greeting() {
+		
+	}
+	
+	public Greeting(String content) {
+		this.setContent(content);
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
